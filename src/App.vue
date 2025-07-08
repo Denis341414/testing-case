3010590900480a436ab15d37867b8dddfbf220c8
Denis341414 <denismanshin2@gmail.com>
2025-07-08 20:34:04 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Header from "./components/Header/ui/Header.vue";
+</script>
+
+<template>
+  <div class="container">
+    <Header />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  padding: 8px 120px;
+  background-color: yellow;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ src/components/Header/ui/Header.vue
@@ -0,0 +1,47 @@
+<script setup></script>
+
+<template>
+  <header class="header">
+    <div class="logo">
+      <img src="../assets/header-logo.svg" alt="logo" />
+    </div>
+    <div class="line-nav">
+      <li class="nav-item">О системе</li>
+      <li class="nav-item">Архитектура</li>
+      <li class="nav-item">Мнения</li>
+      <li class="nav-item">Попробовать</li>
+      <li class="nav-item">Для своих</li>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 24px;
+  height: 68px;
+  width: 100%;
+  background: #ffffff;
+  border-radius: 24px;
+}
+
+.line-nav {
+  display: flex;
+  gap: 32px;
+
+  .nav-item {
+    list-style-type: none;
+    font-family: "Tilda Sans VF";
+    padding: 8px 16px;
+    transition: all 0.3s ease-in-out;
+    cursor: pointer;
+  }
+  .nav-item:hover {
+    background: #1f8b48;
+    border-radius: 40px;
+    color: #ffffff;
+  }
+}
+</style>
