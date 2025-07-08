9245da9babb23665347ad01dbd6a9459bcf605a7
Denis341414 <denismanshin2@gmail.com>
2025-07-08 21:16:44 +0300

    added header ui

--- src/App.vue
+++ src/App.vue
@@ -14,5 +14,12 @@ import Header from "./components/Header/ui/Header.vue";
   background-color: yellow;
   height: 100vh;
   width: 100vw;
+
+  @media (max-width: 1024px) {
+    padding: 8px 10px;
+  }
+  @media (max-width: 480px) {
+    padding: 32px 10px;
+  }
 }
 </style>
--- src/components/Header/ui/Header.vue
+++ src/components/Header/ui/Header.vue
@@ -1,11 +1,26 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+
+const widthScreen = ref(window.screen.width);
+
+const handleResize = () => {
+  widthScreen.value = window.screen.width;
+};
+
+onMounted(() => {
+  window.addEventListener("resize", handleResize);
+});
+</script>
 
 <template>
   <header class="header">
     <div class="logo">
       <img src="../assets/header-logo.svg" alt="logo" />
     </div>
-    <div class="line-nav">
+    <button v-if="widthScreen < 850" class="button-drawer">
+      <img src="../assets/button-img.png" alt="img button" />
+    </button>
+    <div v-if="widthScreen > 850" class="line-nav">
       <li class="nav-item">О системе</li>
       <li class="nav-item">Архитектура</li>
       <li class="nav-item">Мнения</li>
@@ -25,11 +40,18 @@
   width: 100%;
   background: #ffffff;
   border-radius: 24px;
+
+  @media (max-width: 1024px) {
+  }
 }
 
 .line-nav {
   display: flex;
   gap: 32px;
+
+  @media (max-width: 1024px) {
+    gap: 24px;
+  }
 
   .nav-item {
     list-style-type: none;
@@ -43,5 +65,15 @@
     border-radius: 40px;
     color: #ffffff;
   }
+}
+
+.button-drawer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 36px;
+  height: 36px;
+  background: #1f8b48;
+  border-radius: 36px;
 }
 </style>
